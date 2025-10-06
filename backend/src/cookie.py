68812295000eab47b73bb81502c3ed97cf3aef5a
Maksim1Bomba <eeggorr120207@outlook.com>
2025-10-06 20:38:24 +0300

    change structure of backend

--- backend/src/cookie.py
+++ backend/src/cookie.py
@@ -3,12 +3,14 @@ from datetime import datetime, timedelta
 from response import MakeHTTPResponse
 
 
+time_sample = "%a, %d %b %Y %H:%M:%S GMT"
+
 def cookie_check(request, database):
     if 'Cookie' in request.headers:
         cookie = str(dict([tuple(i.split('=')) for i in request.headers['Cookie'].split('; ')])['id'])
         time = database.get_cookie_expire(cookie)
         if time:
-            expire = datetime.strptime(time, "%a, %d %b %Y %H:%M:%S GMT")
+            expire = datetime.strptime(time, time_sample)
             if datetime.utcnow() < expire:
                 return MakeHTTPResponse(200, '').make()
     return MakeHTTPResponse(403, '').make()
@@ -16,5 +18,5 @@ def cookie_check(request, database):
 
 def cookie_create():
     cookie_id = str(uuid.uuid4())
-    expire = (datetime.utcnow() + timedelta(days=7)).strftime("%a, %d %b %Y %H:%M:%S GMT")
+    expire = (datetime.utcnow() + timedelta(days=7)).strftime(time_sample)
     return {'id': cookie_id, 'expire': expire}
