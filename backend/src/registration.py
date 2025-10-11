7ef991b738b4e690786626fc934dbc13218883ae
Maksim1Bomba <eeggorr120207@outlook.com>
2025-10-12 00:12:17 +0300

    http code update

--- backend/src/registration.py
+++ backend/src/registration.py
@@ -1,4 +1,6 @@
 import json
+import http
+
 from password_check import check
 from salt import salt_password, generate_salt
 from response import MakeHTTPResponse
@@ -14,7 +16,7 @@ def reg(request, database):
         password = salt_password(password, salt)
         cookie = cookie_create()
         database.add_user(firstname, lastname, mail, password, salt, cookie)
-        response = MakeHTTPResponse(200, json.dumps(ans)).make(cookie=cookie)
+        response = MakeHTTPResponse(http.HTTPStatus.OK, json.dumps(ans)).make(cookie=cookie)
         return response
-    response = MakeHTTPResponse(400, json.dumps(ans)).make(cookie={})
+    response = MakeHTTPResponse(http.HTTPStatus.NOT_FOUND, json.dumps(ans)).make(cookie={})
     return response
