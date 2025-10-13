b17cfefb37c7a7e874213e693f39b6dbc63f203f
Maksim1Bomba <eeggorr120207@outlook.com>
2025-10-13 20:45:44 +0300

    fix errors

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
-# sudo certbot certonly --standalone  -d bladerunner2049.online --config-dir ./services/nginx/cert/ --email eegorr120207@gmail.com --agree-tos --force-renewal;
-# sudo chown -R a1337:a1337 ./services/nginx/cert;
+sudo certbot certonly --standalone  -d bladerunner2049.online --config-dir ./services/nginx/cert/ --email eegorr120207@gmail.com --agree-tos --force-renewal;
+sudo chown -R a1337:a1337 ./services/nginx/cert;
 
 docker volume create hs4;
 docker build -t postgres ./services/postgres/;
