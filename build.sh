4bdf6feda86764edbcf50d66a0c76074689abae8
Maksim1Bomba <eeggorr120207@outlook.com>
2025-10-13 20:43:49 +0300

    restore postgres

--- build.sh
+++ build.sh
@@ -1,15 +1,9 @@
 # sudo certbot certonly --standalone  -d bladerunner2049.online --config-dir ./services/nginx/cert/ --email eegorr120207@gmail.com --agree-tos --force-renewal;
 # sudo chown -R a1337:a1337 ./services/nginx/cert;
 
-docker volume create hs3;
-docker build -t postgres ./services/psql/;
+docker volume create hs4;
+docker build -t postgres ./services/postgres/;
 docker build -t nginx:1.0 -f ./services/nginx/Dockerfile .;
 docker build -t server:1.0 ./backend;
 
 sudo docker network create -d bridge net;
-
-docker run --name postgres -d -p 5432:5432 -v hs3:/var/lib/postgresql/data postgres;
-# docker exec -i postgres psql -U postgres < ./services/psql/init.sql;
-docker exec -i postgres psql -U boss -d boss < ./services/psql/database.sql;
-docker exec -i postgres psql -U boss -d boss < ./services/psql/films.sql;
-docker stop postgres;
