d615780b5c4f3cc8dcd82b3dd359cfd2521bbe4e
Maksim1Bomba <eeggorr120207@outlook.com>
2025-10-15 21:16:07 +0300

    залет

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-sh set_variables.sh
+source .env
 
 sudo certbot certonly --standalone  -d $DOMAIN_NAME --config-dir ./services/nginx/cert/ --email $USER_EMAIL --agree-tos --force-renewal;
 sudo chown -R $USER_NAME:$USER_NAME ./services/nginx/cert;
--- set_variables.sh
+++
@@ -1,5 +0,0 @@
-set -a
-source .env
-envsubst < ./backend/src/.env > ./backend/src/.env.filled
-mv ./backend/src/.env.filled ./backend/src/.env
-set +a
