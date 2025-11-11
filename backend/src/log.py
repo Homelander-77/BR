34ace3fa3b5663130b85a95aff333cbb1f438e8a
Maksim1Bomba <eeggorr120207@outlook.com>
2025-11-11 20:34:19 +0300

    Log file

---
+++ backend/src/log.py
@@ -0,0 +1,12 @@
+import logging
+
+
+class Log:
+    def __init__(self):
+        self.file_general = '/server/logs/backend/general.log'
+
+    def local(self, file):
+        pass
+
+    def general(self):
+        pass
